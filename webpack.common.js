4d33c7195261a20bdc9ab5d887b5de682b4fd082
John Duff <duff.john@gmail.com>
2021-01-25 14:56:03 -0500

    Set up Hugo with webpack for Netlify

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const AssetsPlugin = require('assets-webpack-plugin')
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, 'src', 'index.js'),
+    app: path.join(__dirname, 'src', 'js', 'app.js')
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist')
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new AssetsPlugin({
+      filename: 'webpack.json',
+      removeFullPathAutoPrefix: true,
+      path: path.join(process.cwd(), 'site/data'),
+      prettyPrint: true
+    })
+  ]
+}
