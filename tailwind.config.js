e84d992ea73a43b7033491adc2bdc65accca3e5c
John Duff <duff.john@gmail.com>
2021-01-25 20:38:01 -0500

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio')
+  ],
+  purge: {
+    content: ['site/layouts/**/*.html']
+  }
+}
